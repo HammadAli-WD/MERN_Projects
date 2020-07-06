f6e10a5b64f717e4f4db40977c2dda927438215a
Hammad-87 <pakswim@gmail.com>
2020-07-06 22:03:22 +0200

    Post method for USERS

---
+++ mernloginreg/routes/Users.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const users = express.Router()
+const cors = require("cors")
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
+const User = require('../models/User')
+users.use(cors())
+
+process.env.SECRET_KEY = 'secret'
+
+users.post('/register', (req, res) => {
+    const today = new Date()
+    const userData = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        password: req.body.password,
+        created: today
+    }
+
+    User.findOne({
+        email: req.body.email
+    })
+})
